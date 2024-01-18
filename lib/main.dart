521d71ab23d1a8613f22a2e3ed4531dfc1b0801f
MOHUND <msaadalharby@stu.kau.edu.sa>
2024-01-18 21:04:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/Start_Screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
